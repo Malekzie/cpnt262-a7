69b05e1781eca17e9ce12e4f4b2eaff1f7b53fe3
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-11-14 23:48:34 -0700

    Created barebones Header component

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<div>
+  <ul class="flex justify-center gap-2 text-2xl bg-slate-600">
+    <a href="/">
+      <li>Home</li>
+    </a><a href="/">
+      <li>Page</li>
+    </a><a href="/">
+      <li>About</li>
+    </a>
+  </ul>
+</div>
