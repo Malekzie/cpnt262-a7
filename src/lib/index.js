2f764a9d3d6a0b5665bf7fd51ae80481dd1e5fa3
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-11-16 16:08:25 -0700

    Added data

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,45 @@
 // place files you want to import through the `$lib` alias in this folder.
+export let dragonData = [
+      {
+        name: "Indus, The Monarch of Flames",
+        src: "/images/fire-dragon.jpg",
+        habitat: "/images/fire-habitat.jpg",
+        gradient: "bg-gradient-to-r from-red-400 via-yellow-400 to-amber-600 bg-clip-text text-transparent"
+      },
+      {
+        name: "Markona, The Guardian of Waves",
+        src: "/images/water-dragon.jpg",
+        gradient: "bg-gradient-to-r from-sky-400 via-cyan-200 to-cyan-800 bg-clip-text text-transparent"
+      },
+      {
+        name: "Terra, The Origin of the Earth",
+        src: "/images/earth-dragon.jpg",
+        gradient: "bg-gradient-to-r from-slate-500 to-slate-800 bg-clip-text text-transparent "
+      },
+      {
+        name: "Mizarrus, The Eternal Storm",
+        src: "/images/wind-dragon.jpg",
+        gradient: "bg-gradient-to-r from-sky-900 via-sky-300 to-sky-700 bg-clip-text text-transparent"
+      },
+      {
+        name: "Gaia, The Breath of Life",
+        src: "/images/plant-dragon.jpg",
+        gradient: "bg-gradient-to-r from-emerald-400 via-lime-300 to-lime-500 bg-clip-text text-transparent"
+      },
+      {
+        name: "Izz'kal, The Creeping Thunder",
+        src: "/images/lightning-dragon.jpg"
+      },
+      {
+        name: "Puff, The First Light",
+        src: "/images/light-dragon.jpg"
+      },
+      {
+        name: "Slade, The Original Dark",
+        src: "/images/dark-dragon.jpg"
+      },
+      {
+        name: "Ao Sol, The Star Forger",
+        src: "/images/cosmic-dragon.jpg"
+      }
+    ];
