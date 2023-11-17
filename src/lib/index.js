3ded5905caec94d3442ade4cbe61a92230a857d2
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-11-16 17:20:52 -0700

    Update index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,42 +4,72 @@ export let dragonData = [
         name: "Indus, The Monarch of Flames",
         src: "/images/fire-dragon.jpg",
         habitat: "/images/fire-habitat.jpg",
-        gradient: "bg-gradient-to-r from-red-400 via-yellow-400 to-amber-600 bg-clip-text text-transparent"
+        textGradient: "",
+        gradient: "bg-gradient-to-r from-red-400 via-yellow-400 to-amber-600 bg-clip-text text-transparent",
+        description: "Indus, A dragon who commands even hellfire to submit to his will. He is the first of his species, the fire dragons, and the strongest among them. Fathered by none and a father of all, his ferocious nature is uncontested amongst draconic kind. "
       },
       {
-        name: "Markona, The Guardian of Waves",
+        name: "Markona, The Guardian of The Seven Seas",
         src: "/images/water-dragon.jpg",
-        gradient: "bg-gradient-to-r from-sky-400 via-cyan-200 to-cyan-800 bg-clip-text text-transparent"
+        habitat: "/images/water-habitat.jpg",
+        textGradient: "",
+        gradient: "bg-gradient-to-r from-sky-400 via-cyan-200 to-cyan-800 bg-clip-text text-transparent",
+        description: "Markona, The twin-bodied serpent of the seas. Willed to Guard them by her creator, she has been doing so for eons. She is the progenitor of the water dragons, and the strongest amongst them. No wave was not seen by her, nor any sea creature not known to her. Legend has it that any surface of water is a mirror to her"
       },
       {
-        name: "Terra, The Origin of the Earth",
+        name: "Terra, The Origin of the Mountains",
         src: "/images/earth-dragon.jpg",
-        gradient: "bg-gradient-to-r from-slate-500 to-slate-800 bg-clip-text text-transparent "
+        habitat: "/images/earth-habitat.jpg",
+        textGradient: "",
+        gradient: "bg-gradient-to-r from-slate-500 to-slate-800 bg-clip-text text-transparent ",
+        description: "Terra, The Shaper of the Earth. In Legend, His steps made valleys, his breath made mountains, and his roar made the earth tremble. He is the progenitor of the earth dragons, and the strongest amongst them. Every mountain is his throne, and every valley his domain and every pebble came from a scratch of his scales."
       },
       {
         name: "Mizarrus, The Eternal Storm",
         src: "/images/wind-dragon.jpg",
-        gradient: "bg-gradient-to-r from-sky-900 via-sky-300 to-sky-700 bg-clip-text text-transparent"
+        habitat: "/images/wind-habitat.jpg",
+        textGradient: "",
+        gradient: "bg-gradient-to-r from-sky-900 via-sky-300 to-sky-700 bg-clip-text text-transparent",
+        description: "Mizzarus, The Fury of the Elements. Once, A protector of a grand and pristine kingdom found in the skies, now but a shadow of his former self. Maddend with grief and rage, He now roams the skies, destroying anything that comes in his path. Nothing is safe in his path and nothing can stop him. Throughout time, He is known as a living natural disaster."
       },
       {
         name: "Gaia, The Breath of Life",
         src: "/images/plant-dragon.jpg",
-        gradient: "bg-gradient-to-r from-emerald-400 via-lime-300 to-lime-500 bg-clip-text text-transparent"
+        habitat: "/images/plant-habitat.jpg",
+        textGradient: "",
+        gradient: "bg-gradient-to-r from-emerald-400 via-lime-300 to-lime-500 bg-clip-text text-transparent",
+        description: "Gaia, Mother of Life, Protector of the Forests. She is the progenitor of the plant dragons, and the strongest amongst them. She is the one who gave life to the forests, and the one who protects them. She is the one who gave the plant dragons their power, and the one who gave them their purpose. She is the one who gave the forests their life, and the one who predestines their death."
       },
       {
         name: "Izz'kal, The Creeping Thunder",
-        src: "/images/lightning-dragon.jpg"
+        src: "/images/lightning-dragon.jpg",
+        habitat: "/images/lightning-habitat.jpg",
+        textGradient: "",
+        gradient: "bg-gradient-to-r from-blue-600 via-sky-200 to-sky-600 bg-clip-text text-transparent",
+        description: "Izz'kal, Lightning Incarnate, Father of Storms. Born from a time of great strife where energy is boundless, incarnated through a boom of thunder. Legend has it that he has crafted every bolt of thunder and is the one who throws them to the ground, slowly building the perfect bolt to pierce the earth."
       },
       {
         name: "Puff, The First Light",
-        src: "/images/light-dragon.jpg"
+        src: "/images/light-dragon.jpg",
+        habitat: "/images/light-habitat.jpg",
+        textGradient: "",
+        gradient: "bg-gradient-to-b from-red-500 to-amber-200 bg-clip-text text-transparent",
+        description: "Puff, The First Light, Defender from the Night. Born from the first spark of creation. He heralds the rising dawn of the new day and the one who banishes the darkness of the night. It is said that his name came from the first puff of light."
       },
       {
-        name: "Slade, The Original Dark",
-        src: "/images/dark-dragon.jpg"
+        name: "Slade, The Nights Origin",
+        src: "/images/dark-dragon.jpg",
+        habitat: "/images/dark-habitat.jpg",
+        textGradient: "",
+        gradient: "bg-gradient-to-r from-gray-900 to-slate-400 bg-clip-text text-transparent",
+        description: "Slade, The Night Bringer, The First Shadow. Born together with Puff, he is the opposite of him. He heralds the coming of the night and the one who banishes the light of the day. It is said that his name came from the first shade of darkness. And his name is the first name feared by humankind."
       },
       {
         name: "Ao Sol, The Star Forger",
-        src: "/images/cosmic-dragon.jpg"
+        src: "/images/cosmic-dragon.jpg",
+        habitat: "/images/cosmic-habitat.jpg",
+        textGradient: "",
+        gradient: "bg-gradient-to-b from-purple-700 via-purple-600 to-fuchsia-400 bg-clip-text text-transparent",
+        description: "Ao Sol, The Star Forger, The First of the Cosmos. Born from the first spark of creation. She is the one who forged the stars and the one who gave them their light. It is said that her name came from the first star that she forged. She roams the stars, looking for an empty void to forge the next star to fill it with light. "
       }
     ];
