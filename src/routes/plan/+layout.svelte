e37528550048325a6eb8736c2b8cef7a918f9edd
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-11-17 11:59:33 -0700

    Minor changes

---
+++ src/routes/plan/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+import { Modal, getModalStore } from '@skeletonlabs/skeleton';
+import { initializeStores } from '@skeletonlabs/skeleton';
+
+initializeStores();
+</script>
+<Modal />
+<slot />
