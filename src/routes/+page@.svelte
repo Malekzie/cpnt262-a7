3813ca8a6079084b4b04a50bfcb71bc380e5a32a
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-11-17 13:37:54 -0700

    Update +page@.svelte

--- src/routes/+page@.svelte
+++ src/routes/+page@.svelte
@@ -1,11 +1,11 @@
 <script>
 import Hero from "../lib/components/Hero.svelte";
 import DragonGallery from "../lib/components/DragonGallery.svelte";
-import Almanac from "../lib/components/Almanac.svelte";
+import HomeAlmanac from "../lib/components/HomeAlmanac.svelte";
 import Prices from "../lib/components/Prices.svelte";
 </script>
 
 <Hero />
 <DragonGallery />
-<Almanac />
+<HomeAlmanac />
 <Prices />
