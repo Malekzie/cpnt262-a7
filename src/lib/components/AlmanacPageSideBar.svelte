f1ee51b8b95a75ed65fbe631349a7c82d039208e
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-11-18 02:32:10 -0700

    Re-doing later with tabs

--- src/lib/components/AlmanacPageSideBar.svelte
+++ src/lib/components/AlmanacPageSideBar.svelte
@@ -8,15 +8,15 @@
   <h1 class="pb-2 ml-5 text-3xl border-solid">Dragon Almanac</h1>
       <div class="h-screen border-t border-r border-black border-solid dark:border-white">
           <ul class="w-full gap-10 py-2 text-center btn-group-vertical">
-              <li><button on:click={indus} type="btn" class="w-full btn indus">Indus</button></li>
-              <li><button on:click={markona} type="btn" class="w-full btn markona">Markona</button></li>
-              <li><button on:click={terra} type="btn" class="w-full btn terra">Terra</button></li>
-              <li><button on:click={mizzarus} type="btn" class="w-full btn mizzarus">Mizzarus</button></li>
-              <li><button on:click={gaia} type="btn" class="w-full btn gaia">Gaia</button></li>
-              <li><button on:click={izzy} type="btn" class="w-full btn izzy">Izz'kal</button></li>
-              <li><button on:click={puff} type="btn" class="w-full btn puff">Puff</button></li>
-              <li><button on:click={slade} type="btn" class="w-full btn slade">Slade</button></li>
-              <li><button on:click={ao} type="btn" class="w-full btn ao">Ao Sol</button></li>
+              <li><button type="btn" class="w-full btn indus">Indus</button></li>
+              <li><button type="btn" class="w-full btn markona">Markona</button></li>
+              <li><button type="btn" class="w-full btn terra">Terra</button></li>
+              <li><button type="btn" class="w-full btn mizzarus">Mizzarus</button></li>
+              <li><button type="btn" class="w-full btn gaia">Gaia</button></li>
+              <li><button type="btn" class="w-full btn izzy">Izz'kal</button></li>
+              <li><button type="btn" class="w-full btn puff">Puff</button></li>
+              <li><button type="btn" class="w-full btn slade">Slade</button></li>
+              <li><button type="btn" class="w-full btn ao">Ao Sol</button></li>
           </ul>
       </div>
 </section>
