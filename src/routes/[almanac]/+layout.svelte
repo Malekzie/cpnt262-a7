1d2fb7bafd1b1a55a671bb9f5c921979424a5f58
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-11-18 15:37:03 -0700

    Deleted unneeded pages

--- src/routes/[almanac]/+layout.svelte
+++ src/routes/[almanac]/+layout.svelte
@@ -1,7 +1,6 @@
 <script>
     import { AppShell } from '@skeletonlabs/skeleton';
     import AlmanacPageSideBar from '../../lib/components/AlmanacPageSideBar.svelte';
-    import AlmanacPageHeader from '../../lib/components/AlmanacPageHeader.svelte';
 </script>
 
 <AppShell>
@@ -9,7 +8,6 @@
         <AlmanacPageSideBar />
     </svelte:fragment>
 	<svelte:fragment slot="pageHeader">
-        <AlmanacPageHeader />
     </svelte:fragment>
 	<slot />
 </AppShell>
