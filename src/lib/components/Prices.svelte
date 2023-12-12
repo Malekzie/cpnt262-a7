18d27f0291454695d4fcb3daf1bbeab20b134ec1
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-12-12 13:27:29 -0700

    Update Prices.svelte

--- src/lib/components/Prices.svelte
+++ src/lib/components/Prices.svelte
@@ -2,7 +2,7 @@
 	import CtaButtons from './CTAButtons.svelte';
 </script>
 
-<section class="h-full my-10 mt-10">
+<section class="my-10 mt-10 ">
 	<h1 class="mb-6 text-3xl font-bold text-center">Pricing</h1>
 	<div class="grid grid-cols-1 gap-6 mx-10 md:grid-cols-3">
 		<div class="p-6 bg-white rounded-lg shadow-lg">
@@ -12,7 +12,7 @@
 				bibendum, sapien mauris commodo velit, vel lacinia velit velit non sapien.
 			</p>
 			<p class="mb-4 text-3xl font-bold text-gray-800">$9.99/month</p>
-			<CtaButtons />
+			<CtaButtons link="/tour/#"/>
 		</div>
 		<div class="p-6 bg-white rounded-lg shadow-lg">
 			<h2 class="mb-4 text-xl font-bold text-black">Pro</h2>
@@ -21,7 +21,7 @@
 				bibendum, sapien mauris commodo velit, vel lacinia velit velit non sapien.
 			</p>
 			<p class="mb-4 text-3xl font-bold text-gray-800">$19.99/month</p>
-			<CtaButtons />
+			<CtaButtons link="/tour/#"/>
 		</div>
 		<div class="p-6 bg-white rounded-lg shadow-lg">
 			<h2 class="mb-4 text-xl font-bold text-black">Premium</h2>
@@ -30,7 +30,7 @@
 				bibendum, sapien mauris commodo velit, vel lacinia velit velit non sapien.
 			</p>
 			<p class="mb-4 text-3xl font-bold text-gray-800">$29.99/month</p>
-			<CtaButtons />
+			<CtaButtons link="/tour/#"/>
 		</div>
 	</div>
 </section>
